70e7b911317b8102044f045af866a0c6f567b6c3
veerendra19codes <veerendragumate@gmail.com>
2024-01-19 02:19:16 +0530

    first commit

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const PORT = process.env.PORT || 3001
 
 const app = express();
 
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   next();
+// });
+
+
 app.use(cors());
 app.use(express.json());
 
